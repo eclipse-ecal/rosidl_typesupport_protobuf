d4e1c92fb1da53ffd83674ec41daf593befa478d
Gonzalo de Pedro <gonzalodepedro@ekumenlabs.com>
2023-12-07 17:10:26 -0300

    Fix linter errors
    Signed-off-by: Gonzo de Pedro <gonzo@depedro.com.ar>

--- rosidl_typesupport_protobuf/include/rosidl_typesupport_protobuf/service_type_support_decl.hpp
+++ rosidl_typesupport_protobuf/include/rosidl_typesupport_protobuf/service_type_support_decl.hpp
@@ -1,19 +1,19 @@
 /* ================================ Apache 2.0 =================================
  *
  * Copyright (C) 2021 Continental
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ================================ Apache 2.0 =================================
  */
 
@@ -21,7 +21,7 @@
 
 #include <rosidl_typesupport_protobuf/visibility_control.h>
 
-//rosidl_service_type_support_t
+// rosidl_service_type_support_t
 #include "rosidl_typesupport_protobuf/rosidl_generator_c_pkg_adapter.hpp"
 
 namespace rosidl_typesupport_protobuf
@@ -36,4 +36,4 @@ template<typename T>
 ROSIDL_TYPESUPPORT_PROTOBUF_PUBLIC
 const rosidl_service_type_support_t * get_service_type_support_handle();
 
-}  // namespace rosidl_typesupport_protobuf_cpp
+}  // namespace rosidl_typesupport_protobuf
